59fd0cc7ab7cf6f70d5718abb5ef5fdcfbc6f6d5
Preadator/Dukman <alexalduk@hotmail.com>
2021-06-15 11:13:49 +1000

    Change CORS appproved header

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use('*', cors())
 app.use(cors({
-    origin: 'https://aalduk-backend.herokuapp.com/auth/signin',
+    origin: 'https://elated-nightingale-5e8652.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'access'],
     maxAge: 600
